3d939e7220daa4a707d57d8e0958b46e33b6bcb9
gouki04 <gouki04@126.com>
2017-04-04 23:45:07 +0800

    更新资源

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -3,14 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Tower
 {
+    [System.Serializable]
+    public struct DialogueData
+    {
+        [SerializeField]
+        public Texture2D Portait;
+
+        [SerializeField]
+        public string Name;
+
+        [SerializeField]
+        public string Text;
+    }
+
     public class Dialogue : TileComponent
     {
         public delegate bool Condition(Player player);
 
         public Condition Conditions;
+
+        public List<DialogueData> BeforeTriggerDialogue = null;
+        public List<DialogueData> TriggerDialogue = null;
+        public List<DialogueData> AfterTriggerDialogue = null;
 
         public override bool CheckCanTrigger(Player player)
         {
@@ -19,6 +37,14 @@ namespace Tower
             }
 
             return true;
+        }
+
+        public override IEnumerator OnBeforeTrigger(Player player)
+        {
+            if (BeforeTriggerDialogue != null) {
+
+            }
+            return null;
         }
 
         public override IEnumerator OnTrigger(Player player)
